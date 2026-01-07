c00c8e97895c60d5d26fd7ce47cb75afc5a9a3cc
frolesti <frolesti4@gmail.com>
2026-01-07 13:29:43 +0100

    fix(web): use en keys for catalan translations in InstrumentCard to satisfy libui types

--- apps/web/src/components/InstrumentCard/InstrumentCard.tsx
+++ apps/web/src/components/InstrumentCard/InstrumentCard.tsx
@@ -29,28 +29,28 @@ export const InstrumentCard = ({ instrument, onClick }: InstrumentCardProps) => 
     {
       kind: 'text',
       label: t({
-        ca: 'Autors'
+        en: 'Autors'
       }),
       text: instrument.details.authors?.join(', ')
     },
     {
       kind: 'text',
       label: t({
-        ca: 'Descripció'
+        en: 'Descripció'
       }),
       text: instrument.details.description
     },
     {
       kind: 'text',
       label: t({
-        ca: 'Edició'
+        en: 'Edició'
       }),
       text: instrument.internal?.edition.toString()
     },
     {
       kind: 'text',
       label: t({
-        ca: 'Idiomes'
+        en: 'Idiomes'
       }),
       text: instrument.supportedLanguages
         .map((language) => {
@@ -72,7 +72,7 @@ export const InstrumentCard = ({ instrument, onClick }: InstrumentCardProps) => 
     {
       kind: 'text',
       label: t({
-        ca: 'Llicència'
+        en: 'Llicència'
       }),
       text: license?.name ?? 'NA',
       tooltip: (
@@ -88,10 +88,10 @@ export const InstrumentCard = ({ instrument, onClick }: InstrumentCardProps) => 
             <p>
               {license?.isOpenSource
                 ? t({
-                    ca: 'Aquesta és una llicència lliure i de codi obert'
+                    en: 'Aquesta és una llicència lliure i de codi obert'
                   })
                 : t({
-                    ca: 'Aquesta no és una llicència lliure i de codi obert'
+                    en: 'Aquesta no és una llicència lliure i de codi obert'
                   })}
             </p>
           </Tooltip.Content>
@@ -102,20 +102,20 @@ export const InstrumentCard = ({ instrument, onClick }: InstrumentCardProps) => 
       href: instrument.details.referenceUrl,
       kind: 'link',
       label: t({
-        ca: 'Enllaç de referència'
+        en: 'Enllaç de referència'
       })
     },
     {
       href: instrument.details.sourceUrl,
       kind: 'link',
       label: t({
-        ca: 'Enllaç al codi font'
+        en: 'Enllaç al codi font'
       })
     },
     {
       kind: 'text',
       label: t({
-        ca: 'Etiquetes'
+        en: 'Etiquetes'
       }),
       text: instrument.tags.join(', ')
     }
